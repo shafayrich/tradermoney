e9e2e8dd8e4df8b1dc717102ff5c34efb92c5d5c
shafayrich <khattakshafay@gmail.com>
2026-04-25 22:12:19 +0300

    Bump app version to 1.0.7

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 # ---------- App Version (bump this on each release) ----------
-APP_VERSION = "1.0.6"
+APP_VERSION = "1.0.7"
 
 # ---------- Flask App ----------
 app = Flask(__name__)
